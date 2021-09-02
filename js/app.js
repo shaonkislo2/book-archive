98d0119e4f13d08a3f3deda76e787dbf797c5e53
shaonkislo <shaon6340@agmail.com>
2021-09-02 21:49:04 +0600

    final 09.48 pm

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const searchBook = () => {
     const searchText = searchField.value; 
     console.log(searchText);
     searchField.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchText}`
+    const url = `https://openlibrary.org/search.json?q=${searchText}`
    fetch(url)
    .then(res => res.json())
    .then(data => displaySerachResult(data.docs));
